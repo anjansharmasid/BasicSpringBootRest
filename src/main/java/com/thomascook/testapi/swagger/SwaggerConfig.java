c89079747d92ea0324059a20ad0d4d07baab9461
anjansharmasid <anjansharmasid@gmail.com>
2018-07-19 17:00:14 +0100

    Minor fix
    Minor fix to the swagger url.

--- src/main/java/com/thomascook/testapi/swagger/SwaggerConfig.java
+++ src/main/java/com/thomascook/testapi/swagger/SwaggerConfig.java
@@ -16,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {   
-	// http://localhost:8080/swagger-ui.html
+	// http://localhost:8080/test/swagger-ui.html
     @Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
